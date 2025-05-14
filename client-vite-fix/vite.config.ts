3930013a77e4cf698515a73bfb88737e4e99cdc7
wade <babacarw825@gmail.com>
2025-05-14 05:29:45 +0200

    Rétablissement de la config Vite d'origine (minify: 'terser')

--- client-vite-fix/vite.config.ts
+++ client-vite-fix/vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: false
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        pure_funcs: ['eval']
+      }
+    }
   }
 })
